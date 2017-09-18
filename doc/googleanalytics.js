27dce4a0f86951898924f9a7b2be1eaff7e227ca
Mathias Dalheimer <md@gonium.net>
2017-09-18 16:53:04 +0200

    added google analytics to track usage

---
+++ doc/googleanalytics.js
@@ -0,0 +1,10 @@
+<!-- Global Site Tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-1705081-2"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments)};
+  gtag('js', new Date());
+
+  gtag('config', 'UA-1705081-2');
+</script>
+
